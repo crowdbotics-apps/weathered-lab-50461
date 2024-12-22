bc3c1eac10ee5191772166406a568e40ae850a93
Crowdbotics <team@crowdbotics.com>
2024-12-22 09:10:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-lab-50461",
+  url: "https://weathered-lab-50461.botics.co",
+  api: "https://weathered-lab-50461.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
